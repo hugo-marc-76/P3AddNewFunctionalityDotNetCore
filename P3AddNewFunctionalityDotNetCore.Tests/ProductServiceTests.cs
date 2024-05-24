5659fc95c17bfa428db2b0b1a2bc930c7e097dc8
Hugo MARC <gogodu76diablo@gmail.com>
2024-05-24 13:07:06 +0200

    create ProductServiceTest
    complétion du code de ProductServiceTest pour réaliser les test.
    
    complétion de ProductViewModel pour réaliser les  test.
    
    ajout d'une classe permettant de vérifier si Price contient bien un nombre.

--- P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,26 +1,88 @@
-﻿using Xunit;
+﻿using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
     public class ProductServiceTests
     {
-        /// <summary>
-        /// Take this test method as a template to write your test method.
-        /// A test method must check if a definite method does its job:
-        /// returns an expected value from a particular set of parameters
-        /// </summary>
-        [Fact]
-        public void ExampleMethod()
+        private ProductViewModel product;
+
+        public ProductServiceTests()
         {
-            // Arrange
-
-            // Act
-
-
-            // Assert
-            Assert.Equal(1, 1);
+            product = new ProductViewModel();
         }
 
-        // TODO write test methods to ensure a correct coverage of all possibilities
+        [Fact]
+        public void TestMissingPrice()
+        {
+            product.Name = "Product1";
+            product.Stock = "2";
+            product.Price = null;
+            Assert.False(ValidateModel(product));
+            Assert.Equal("MissingPrice", GetFirstErrorMessage(product));
+        }
+
+        [Fact]
+        public void TestMissingName()
+        {
+            product.Name = null;
+            product.Price = "25.0";
+            product.Stock = "10";
+            Assert.False(ValidateModel(product));
+            Assert.Equal("MissingName", GetFirstErrorMessage(product));
+        }
+
+        [Fact]
+        public void TestPriceNotANumber()
+        {
+            product.Name = "Product2";
+            product.Price = "K";
+            product.Stock = "10";
+            Assert.False(ValidateModel(product));
+            Assert.Equal("PriceNotANumber", GetFirstErrorMessage(product));
+        }
+
+        [Fact]
+        public void TestPriceNotGreaterThanZero()
+        {
+            product.Name = "Product3";
+            product.Price = "0.0";
+            product.Stock = "19";
+            Assert.False(ValidateModel(product));
+            Assert.Equal("PriceNotGreaterThanZero", GetFirstErrorMessage(product));
+        }
+
+
+
+        [Fact]
+        public void TestQuantityNotGreaterThanZero()
+        {
+            product.Name = "Product4";
+            product.Price = " 10.0";
+            product.Stock = "0";
+            Assert.False(ValidateModel(product));
+            Assert.Equal("QuantityNotGreaterThanZero", GetFirstErrorMessage(product));
+        }
+
+
+
+        // Ajoutez ici d'autres méthodes pour tester les autres cas...
+
+        private bool ValidateModel(object model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(model, null, null);
+            return Validator.TryValidateObject(model, ctx, validationResults, true);
+        }
+
+        private string GetFirstErrorMessage(object model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, ctx, validationResults, true);
+            return validationResults[0].ErrorMessage;
+        }
     }
 }
--- P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 {
@@ -7,14 +9,34 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
         [BindNever]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "MissingName")]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
 
+        [Required(ErrorMessage = "MissingQuantity")]
+        [Range(1, int.MaxValue, ErrorMessage = "QuantityNotGreaterThanZero")]
         public string Stock { get; set; }
 
+        [Required(ErrorMessage = "MissingPrice")]
+        [IsNumber(ErrorMessage = "PriceNotANumber")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "PriceNotGreaterThanZero")]
         public string Price { get; set; }
     }
+
+    public class IsNumberAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is string strValue && !double.TryParse(strValue, NumberStyles.Float, CultureInfo.InvariantCulture, out _))
+            {
+                return new ValidationResult("PriceNotANumber");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
 }
